8de6a08819398aceb8203e5c4b4906360499ddbe
jeanvit <jeanvitordepaulo@gmail.com>
2017-04-28 11:40:36 -0300

    Friday update

--- main.cpp
+++ main.cpp
@@ -19,9 +19,9 @@ auto main() -> int{
 	vector<uint> whiteDotsManualRVE,whiteDotsAutoRVE;
 	string nameFirstImage, nameSecondImage;
 	cout << "Enter the name of first image" <<std::endl;
-	cin >> nameFirstImage;
+	//cin >> nameFirstImage;
 	cout << "Enter the name of second image" <<std::endl;
-	cin >> nameSecondImage;
+	//cin >> nameSecondImage;
 	firstImage=imread(/*nameFirstImage.c_str()*/"a.png", IMREAD_COLOR);
 	secondImage=imread(/*nameSecondImage.c_str()*/"c.png",IMREAD_COLOR);
 	//absdiff(firstImage, secondImage,manualRVE);
@@ -29,28 +29,34 @@ auto main() -> int{
 	namedWindow( "Main window", CV_WINDOW_KEEPRATIO );
 	imshow("Main window", manualRVE);
 	whiteDotsManualRVE=whiteDotsVector(manualRVE);
-	//whiteDotsAutoRVE=whiteDotsVector(autoRVE);
+	whiteDotsAutoRVE=whiteDotsVector(firstImage);
 
 	for (auto i=0;i<firstImage.rows;i++)
 		cout<< whiteDotsManualRVE.at(i) <<endl;
-
-	rootMeanSquareDeviation(whiteDotsManualRVE,whiteDotsManualRVE);
+// TO DO
+	//CORRECT WHITE DOTS AUTO RVE DATA TO A CORRECT FILE
+	rootMeanSquareDeviation(whiteDotsManualRVE,whiteDotsAutoRVE);
 	waitKey(0);
 }
 
 double rootMeanSquareDeviation(const vector<uint> autoWhiteDots,const vector<uint> manualWhiteDots){
-	uint vectorSize = autoWhiteDots.size();
-	cout<<"i"<<vectorSize<<endl;
+	int vectorSize = autoWhiteDots.size();
+	cout<<"Tamanho do vetor: "<<vectorSize<<endl;
+
 	double sum=0;
 	double result=0;
 	/* RMSD COMPUTATION*/
 	/*x= root square of 1/n sum 1 until n (pointA-pointB) */
 	for (uint i = 0;i<vectorSize;i++){
-		//sum = sum + static_cast<double>(autoWhiteDots.at(i));
 		sum=sum+pow(static_cast<double>(autoWhiteDots.at(i))-static_cast<double>(manualWhiteDots.at(i)),2);
+
+
+
+
 	}
 	sum=sum*1/vectorSize;
 	result=sqrt(sum);
+	cout<<"result "<<result<<endl;
 	return result;
 }
 
