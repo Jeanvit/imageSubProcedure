511d37cd70e7c1004fd4891d392a1baf9dc3f04d
jeanvit <jeanvitordepaulo@gmail.com>
2017-05-02 11:00:51 -0300

    first complete version

--- main.cpp
+++ main.cpp
@@ -25,18 +25,27 @@ auto main() -> int{
 	//cin >> nameSecondImage;
 	cout << "Enter the name of the RVE image" <<std::endl;
 	//cin >> nameGeneratedRVE;
-	firstImage=imread(/*nameFirstImage.c_str()*/"a.png", IMREAD_COLOR);
-	secondImage=imread(/*nameSecondImage.c_str()*/"c.png",IMREAD_COLOR);
-	//autoRVE=imread(/*nameSecondImage.c_str()*/"c.png",IMREAD_COLOR);
+	firstImage=imread(/*nameFirstImage.c_str()*/"y.png", IMREAD_COLOR);
+	secondImage=imread(/*nameSecondImage.c_str()*/"x.png",IMREAD_COLOR);
+	autoRVE=imread(/*nameSecondImage.c_str()*/"z.png",IMREAD_COLOR);
 	//absdiff(firstImage, secondImage,manualRVE);
 	manualRVE = firstImage - secondImage;
 	namedWindow( "Main window", CV_WINDOW_KEEPRATIO );
 	imshow("Main window", manualRVE);
 	whiteDotsManualRVE=whiteDotsVector(manualRVE);
-	whiteDotsAutoRVE=whiteDotsVector(firstImage);
+	whiteDotsAutoRVE=whiteDotsVector(autoRVE);
+
+	vector<int> compression_params;
+	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+	compression_params.push_back(9);
+
+	imwrite("output.png", manualRVE, compression_params);
 
 	for (auto i=0;i<firstImage.rows;i++)
 		cout<< whiteDotsManualRVE.at(i) <<endl;
+
+	for (auto i=0;i<firstImage.rows;i++)
+			cout<< whiteDotsAutoRVE.at(i) <<endl;
 // TO DO
 	//CORRECT WHITE DOTS AUTO RVE DATA TO A CORRECT FILE
 	rootMeanSquareDeviation(whiteDotsManualRVE,whiteDotsAutoRVE);
@@ -45,7 +54,9 @@ auto main() -> int{
 
 double rootMeanSquareDeviation(const vector<uint> autoWhiteDots,const vector<uint> manualWhiteDots){
 	int vectorSize = autoWhiteDots.size();
+	int manualVectorSize = manualWhiteDots.size();
 	cout<<"Tamanho do vetor: "<<vectorSize<<endl;
+	cout<<"Tamanho do vetor manual: "<<manualVectorSize<<endl;
 
 	double sum=0;
 	double result=0;
@@ -53,9 +64,6 @@ double rootMeanSquareDeviation(const vector<uint> autoWhiteDots,const vector<uin
 	/*x= root square of 1/n sum 1 until n (pointA-pointB) */
 	for (uint i = 0;i<vectorSize;i++){
 		sum=sum+pow(static_cast<double>(autoWhiteDots.at(i))-static_cast<double>(manualWhiteDots.at(i)),2);
-
-
-
 
 	}
 	sum=sum*1/vectorSize;
@@ -70,12 +78,14 @@ vector<uint> whiteDotsVector(const Mat img){
 	cvtColor(img,convertedImg,CV_BGR2GRAY);
 	namedWindow( "test window", CV_WINDOW_KEEPRATIO );
 	imshow("test window",convertedImg);
-	for (auto i=0;i<img.rows;i++)
-		for (auto j=0;j<img.cols;j++){
-			if (convertedImg.at<uchar>(i,j)!=0){
-				whiteDotsArray.push_back(j);
+	for (auto i=0;i<img.rows;i++){
+		whiteDotsArray.push_back(0);
+		for (auto j=0;j<img.cols;j++){				/* This guarantees that the whitedots vector will have the same size as the image */
+			if (convertedImg.at<uchar>(i,j)!=0){	/* even if there's no enough lines with white dots */
+				whiteDotsArray.at(whiteDotsArray.size()-1)=j;
 				break;
 			}
 		}
+	}
 	return whiteDotsArray;
 }
